09948d01a3c28a9a2398898f39f354a14e2a9891
Ethan Henderson <ehenderson@umail.ucsb.edu>
2017-11-15 16:51:25 -0800

    EH/TC added client class

---
+++ src/edu/ucsb/cs56/networking/expression_evaluator/Client.java
@@ -0,0 +1,15 @@
+package edu.ucsb.cs56.networking.expression_evaluator;
+import java.lang.*;
+import java.io.*;
+import java.net.*;
+
+public class Client {
+
+	int portNum;
+	Socket clientsock;
+
+	public void runClient() {
+
+
+	public static void main (String args[]) {
+
